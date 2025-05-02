154f01b0ca2cd7e96e604b1b43bfaee9c432c69d
zhangTWN <zyrenyy@gmail.com>
2025-05-02 18:48:47 +0800

    v2.3.1
    - 新增Facebook連結
    - 更改連結按鈕排版

--- assets/mods/footer.js
+++ assets/mods/footer.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 建立 style 樣式
     const style = document.createElement("style");
     style.textContent = `
       footer {
@@ -10,16 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
             text-shadow: 1px 1px 3px rgba(0,0,0,0.6);
             z-index: 2;
             position: relative;
+            text-align: center; 
       }
     `;
-    document.head.appendChild(style); // 加入 <head>
+    document.head.appendChild(style); 
   
-    // 建立 footer 元素
     const year = new Date().getFullYear();
     const footer = document.createElement("footer");
     footer.innerHTML = `<p>&copy; ${year} The Next Generation Taiwanese - TNGT. All rights reserved.</p>`;
   
-    // 加入 <body>
     document.body.appendChild(footer);
   });
   
