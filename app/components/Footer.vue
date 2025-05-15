7131bd66eca1cb1ce53c399e617b32621460da79
charles <abcd854884@gmail.com>
2025-05-15 22:04:02 +0800

    🎨 style: Footer 排版調整

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <footer class="relative z-10 mt-auto py-4 text-center text-sm">
+  <footer class="relative z-10 mt-auto p-4 text-center text-sm">
     <p>
-      版權所有 &copy; {{ year }} 台灣未來的主人們 The Next Generation Taiwanese
-      | MIT License
+      版權所有 &copy; {{ year }} 台灣未來的主人們 <br />
+      The Next Generation Taiwanese | MIT License
     </p>
   </footer>
 </template>
