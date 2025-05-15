e8cbd7e213a3c3b6a7952feeffc9f9bbadc045d0
charles <abcd854884@gmail.com>
2025-05-15 19:12:19 +0800

    📝 docs: 修訂版權聲明與 Footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -5,8 +5,8 @@
 <template>
   <footer class="relative z-10 mt-auto py-4 text-center text-sm">
     <p>
-      &copy; {{ year }} The Next Generation Taiwanese - TNGT. All rights
-      reserved.
+      版權所有 &copy; {{ year }} 台灣未來的主人們 The Next Generation Taiwanese
+      | MIT License
     </p>
   </footer>
 </template>
