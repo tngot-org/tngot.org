49aef4fc60b35a39059b8dae44b9a9e391463e14
charles <abcd854884@gmail.com>
2025-05-16 03:31:37 +0800

    🎨 style: 首頁字體與按鈕大小 RWD 最佳化

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,12 +20,12 @@
       class="mt-6 rounded-[10px] bg-transparent px-4 py-2 text-white shadow-lg backdrop-blur-sm md:px-8 md:py-4"
     >
       <h1
-        class="text-[3.5rem] font-extrabold text-white text-shadow-gray-800 text-shadow-md md:text-[3.5rem]"
+        class="text-[2rem] font-extrabold text-white text-shadow-gray-800 text-shadow-md sm:text-[2.5rem] md:text-[3.5rem]"
       >
         台灣未來的主人們
       </h1>
       <p
-        class="mt-1 text-[1.5rem] text-white text-shadow-gray-800 text-shadow-md md:mt-2 md:text-[1.3rem]"
+        class="mt-1 text-[1rem] text-white text-shadow-gray-800 text-shadow-md sm:text-[1.75rem] md:mt-2 md:text-[2rem]"
       >
         The Next Generation Taiwanese - TNGT
       </p>
@@ -33,14 +33,14 @@
 
     <!-- 副標題區塊 -->
     <div
-      class="text-md mt-2 rounded-[10px] bg-transparent px-4 py-2 leading-relaxed text-white shadow-lg backdrop-blur-md text-shadow-gray-500 text-shadow-md md:px-8 md:py-4"
+      class="text-md mt-2 rounded-[10px] bg-transparent px-4 py-2 leading-relaxed break-keep text-white shadow-lg backdrop-blur-md text-shadow-gray-500 text-shadow-md sm:mx-4 sm:text-2xl"
     >
-      <p class="mt-1 leading-relaxed md:mt-2 md:text-base">
+      <p class="mt-1 leading-relaxed md:mt-2">
         我們期望發展成為一個真正的公民團體；
         <br />
         用於代表青少年們參與大大小小的公共事務。
       </p>
-      <p class="mt-1 leading-relaxed md:mt-2 md:text-base">
+      <p class="mt-1 leading-relaxed md:mt-2">
         我們追求民主與自由，堅信青年力量能驅動社會進步；<br />
         關注民主、教育與性平等議題，凝聚學生聲音，打造屬於我們的聲音平台。
       </p>
@@ -48,16 +48,20 @@
 
     <!-- Discord 成員數量 -->
     <DiscordMembers
-      class="mt-8 mb-2 text-lg font-bold text-white text-shadow-gray-800 text-shadow-md md:mt-20 md:text-xl"
+      class="my-4 text-xl font-bold text-white text-shadow-gray-800 text-shadow-md md:text-2xl 2xl:text-3xl"
     />
 
     <!-- Discord 按鈕 -->
     <UButton
       icon="ic:baseline-discord"
-      size="lg"
+      size="xl"
       to="https://discord.com/invite/BCFcxMU6zV"
       target="_blank"
-      class="relative mx-2.5 overflow-hidden rounded-md bg-gradient-to-r from-[#7289da] to-[#99aab5] px-6 py-3 font-bold text-white shadow-lg transition-transform duration-300 hover:scale-110 md:px-8 md:py-4"
+      class="relative overflow-hidden rounded-md bg-gradient-to-r from-[#7289da] to-[#99aab5] px-7 py-5 text-xl font-bold"
+      :ui="{
+        base: 'lg:px-8 lg:py-6 lg:text-2xl',
+        leadingIcon: 'lg:p-4'
+      }"
     >
       加入 Discord
       <div
@@ -66,14 +70,20 @@
     </UButton>
 
     <!-- 按鈕群組 -->
-    <div class="mt-12 flex flex-wrap justify-center gap-2 md:mt-12 md:gap-4">
+    <div
+      class="mt-12 inline-grid grid-cols-2 gap-6 p-6 sm:grid-cols-3 md:mt-12 xl:grid-cols-6"
+    >
       <UButton
         v-for="button in buttons"
         :key="button.to"
         :to="button.to"
         :icon="button.icon"
         size="xl"
-        class="m-1 px-4 py-2 font-bold md:m-2 md:px-6 md:py-3"
+        class="px-4 py-3 text-xl font-bold"
+        :ui="{
+          base: 'lg:px-6 lg:py-4.5 lg:text-xl',
+          leadingIcon: 'lg:p-4'
+        }"
       >
         {{ button.text }}
       </UButton>
