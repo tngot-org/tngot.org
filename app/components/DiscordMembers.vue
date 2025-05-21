fc35cc7015738d3e4745c29b22550a199d0eff04
charles <abcd854884@gmail.com>
2025-05-22 05:40:28 +0800

    🔨 refactor: 最佳化動畫載入造成的水合問題

--- app/components/DiscordMembers.vue
+++ app/components/DiscordMembers.vue
@@ -5,9 +5,11 @@
   const rounded = useTransform(() => Math.round(count.get()));
   let controls: ReturnType<typeof animate> | undefined;
 
-  const { status, data } = await useGetDiscordMembersCount();
-  if (status) {
-    controls = animate(count, data.value as number, { duration: 1.5 });
+  const { memberCount, isLoading, isError, isSuccess } =
+    await useGetDiscordMembersCount();
+
+  if (isSuccess.value) {
+    controls = animate(count, memberCount.value as number, { duration: 1.5 });
   }
 
   onUnmounted(() => {
@@ -16,17 +18,22 @@
 </script>
 
 <template>
-  <div v-if="status">
-    <p>
-      目前社群成員:
-      <span v-if="status">
-        <RowValue :value="rounded" />
-      </span>
-      <!-- 避免 hydration -->
-      <span v-else>0</span>
-      人
-    </p>
+  <div>
+    <template v-if="isLoading">
+      <span>載入中...</span>
+    </template>
+    <template v-else-if="isError">
+      <span>取得 Discord 成員數失敗</span>
+    </template>
+    <template v-else-if="isSuccess">
+      <p>
+        目前社群成員:
+        <ClientOnly>
+          <RowValue :value="rounded" />
+          <template #fallback> 0 </template>
+        </ClientOnly>
+        人
+      </p>
+    </template>
   </div>
-
-  <div v-else />
 </template>
--- app/composables/useGetDiscordMembersCount.ts
+++ app/composables/useGetDiscordMembersCount.ts
@@ -2,28 +2,32 @@ interface DiscordInviteResponse {
   approximate_member_count: number;
 }
 
-export default async function () {
+export default async function useGetDiscordMembersCount() {
   const memberCount = useState<number | null>(
     'discord-member-count',
     () => null
   );
-  const status = useState<boolean>('discord-status', () => false);
 
   const inviteCode = useRuntimeConfig().public.discordInviteCode;
   const url = `https://discord.com/api/v9/invites/${inviteCode}?with_counts=true&with_expiration=true&with_permissions=true`;
 
-  const { data, error } = await useFetch<DiscordInviteResponse>(url, {
+  const { data, error, pending } = await useFetch<DiscordInviteResponse>(url, {
     key: 'discord-member-count',
     cache: 'reload'
   });
 
-  if (data.value) {
-    memberCount.value = data.value.approximate_member_count ?? null;
-    status.value = !error.value;
-  }
+  memberCount.value = data.value?.approximate_member_count ?? null;
+
+  const isLoading = computed(() => pending.value);
+  const isError = computed(() => !!error.value);
+  const isSuccess = computed(
+    () => !pending.value && !error.value && memberCount.value !== null
+  );
 
   return {
-    status,
-    data: memberCount
+    memberCount,
+    isLoading,
+    isError,
+    isSuccess
   };
 }
